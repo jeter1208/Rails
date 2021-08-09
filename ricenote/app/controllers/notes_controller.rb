52d6b2e361a7090c92c6bf17189825cf8bec9058
jeter <fl526128@gmail.com>
2021-08-09 16:58:18 +0800

    ricenote

--- ricenote/app/controllers/notes_controller.rb
+++ ricenote/app/controllers/notes_controller.rb
@@ -2,5 +2,17 @@ class NotesController < ApplicationController
     def index
     end
     def new
-    end    
+        @note = Note.new
+    end
+    def create
+      title = params[:title]
+      content = params[:content]
+      note = Note.new(title: title,content: content) 
+    #   render html:params[:title]
+      if note.save
+        redirect_to "/notes"
+      else
+         ##待處理 
+      end
+    end        
 end
