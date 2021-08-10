89905b957f390ddb0ddc4fb120222f4dbbf109bc
jeter <fl526128@gmail.com>
2021-08-10 15:42:20 +0800

    ricenote

--- ricenote/app/controllers/notes_controller.rb
+++ ricenote/app/controllers/notes_controller.rb
@@ -5,13 +5,21 @@ class NotesController < ApplicationController
         @note = Note.new
     end
     def create
-      title = params[:title]
-      content = params[:content]
-      note = Note.new(title: title,content: content) 
+    #   reder html: params[:note]
+    #   note = Note.new(params[:note]) 
+    #   title = params[:title]
+    #   content = params[:content]
+    #   note = Note.new(title: title,content: content) 
     #   render html:params[:title]
-      if note.save
+        # strong Prameter 強參數 新增白名單
+       clean_note =params.require(:note).permit(:title,:content)
+       @note = Note.new(clean_note)
+      if @note.save
         redirect_to "/notes"
       else
+        # reder html: "123"
+        render :new
+        # redirect_to "/notes/new"
          ##待處理 
       end
     end        
