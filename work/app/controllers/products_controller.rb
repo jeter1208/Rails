3d79df06ba1768c85168e01ce6525fda3d6abbd3
jeter <fl526128@gmail.com>
2021-08-13 11:29:58 +0800

    新增商品

--- work/app/controllers/products_controller.rb
+++ work/app/controllers/products_controller.rb
@@ -1,5 +1,21 @@
 class ProductsController < ApplicationController
-#   def index
-#     @products = product.order(id :dsec)  
-#   end
+  def index
+    @products = Product.all
+    # order(id :dsec)  
+  end
+  def new
+    @product = Product.new 
+  end
+ def create
+    clean_product = params.require(:product).permit(:title,:price,:describe,:is_active)
+    @product = Product.new(clean_product)
+    if @product.save
+        redirect_to "/products"
+      else
+        render :new
+      end
+ end
+ def show
+    @products = Product.find(params[:id])
+ end
 end
