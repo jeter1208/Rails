c0547a52153f2546991a3664a820b45443519350
jeter <fl526128@gmail.com>
2021-08-14 17:25:19 +0800

    商品練習一

--- work/app/controllers/products_controller.rb
+++ work/app/controllers/products_controller.rb
@@ -1,22 +1,52 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only:[:show,:update,:edit,:destory]
+
   def index
-    @products = Product.all
-    # order(id :dsec)  
+    @products = Product.order(id: :desc)   
   end
+
   def new
     @product = Product.new 
   end
- def create
-    clean_product = params.require(:product).permit(:title,:price,:describe,:is_active)
-    @product = Product.new(clean_product)
+
+  def create
+    # clean_product = params.require(:product).permit(:title,:price,:describe,:is_active)
+    @product = Product.new(product_params)
     if @product.save
         redirect_to "/products"
       else
         render :new
       end
- end
- def show
+  end
+
+  def show
     @product = Product.find(params[:id])
- end
+  end  
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to "/products"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title,:price,:describe,:is_active)
+  end
+  def find_product
+    @product = Product.find(params[:id])
+  end
 end
-# Hero.where("age >= 10 and age <= 25")
+
